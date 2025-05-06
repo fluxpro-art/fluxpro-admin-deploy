301f6ae3294c3b588b73002c1eade9f58116bd8e
Daniel Campos <anielcampos991@gmail.com>
2025-05-06 18:06:36 -0300

    Atualiza├º├úo autom├ítica do projeto FluxPro

---
+++ corrigir_push_lfs.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:44d9bfef8161e92a43d8e57ccf86ef8e63eb0513d963c6018b0c94788d1c5578
+size 798
---
+++ push_fluxpro.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6eb446c4363ea35190d7dba83381c55a4ecc0300b6acece7f40fa1fd168a0bf8
+size 397
